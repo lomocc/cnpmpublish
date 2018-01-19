35b5c6f41893630932070d3d60bcd526a83290b0
Vincent <const.lomo@gmail.com>
2018-01-19 11:29:38 +0800

    refactor

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 
 var process = require('child_process');
+
+var npm_registry = 'http://registry.npmjs.org';
+// const cnpm_registry = 'https://registry.npm.taobao.org';
+var cmd_get_registry = `npm config get registry`;
+
 //直接调用命令
-process.exec('npm config set registry http://registry.npmjs.org && npm publish && npm config set registry https://registry.npm.taobao.org', function(error, stdout, stderr) {
+process.exec(cmd_get_registry, function(error, stdout, stderr) {
     if (error !== null) {
         console.log('[error]' + error);
     }else {
-        console.log('[success]' + stdout);
+        var cnpm_registry = stdout.replace(/\s/g, '');
+        var cmd_publish = `npm config set registry ${npm_registry} && npm publish && npm config set registry ${cnpm_registry} && ${cmd_get_registry}`;
+        process.exec(cmd_publish, function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('[error]' + error);
+            }else {
+                console.log('[success]', stdout);
+            }
+        });
     }
 });
